ca7359f46605e336bc4c3f0204bc4e3f4ac7e009
superpuper777 <superfevrik@gmail.com>
2021-03-24 00:14:20 +0300

    feat: add the most useful material modules

--- rslang/src/app/material/material.module.ts
+++ rslang/src/app/material/material.module.ts
@@ -1,8 +1,51 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule, MatChipInputEvent } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+  ],
+  exports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+  ],
 })
 export class MaterialModule {}
