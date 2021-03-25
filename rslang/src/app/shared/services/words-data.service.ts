825f956b163b09f5e39051b30355f3c6363d72bf
Anna Tolstova <anna_tolstova@hotmail.com>
2021-03-25 09:59:30 +0200

    fix: change model name

--- rslang/src/app/shared/services/words-data.service.ts
+++ rslang/src/app/shared/services/words-data.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 
 import { WORDS_DATA, WORDS_API_URL } from '../constants/constants';
 
-import { IWord } from '../models/word.model';
+import { Word } from '../models/word.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WordsDataService {
   public GetWords() {
-    const apiWords: IWord[] = [];
+    const apiWords: Word[] = [];
     WORDS_DATA.forEach(function (word) {
       (word.image = WORDS_API_URL + word.image),
         (word.audio = WORDS_API_URL + word.audio),
