ba9649ebb7f4ffdfc912d3baaa5e4740bd084389
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-03-26 02:34:04 +0300

    feat: add list words

--- rslang/src/app/ebook/components/ebook-page/ebook-page.component.ts
+++ rslang/src/app/ebook/components/ebook-page/ebook-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+
+import { GetWordDataService } from '../../../shared/services/word.service';
+import { Word } from '../../../shared/models/word.model';
 
 @Component({
   selector: 'app-ebook-page',
   templateUrl: './ebook-page.component.html',
   styleUrls: ['./ebook-page.component.scss'],
 })
-export class EbookPageComponent {
+export class EbookPageComponent implements OnInit, OnDestroy {
+  collectionWord!: Word;
+  wordSubscription!: Subscription;
+  params: HttpParams = new HttpParams()
+    .set('group', String(1))
+    .set('page', String(10));
+  constructor(private getWordDataService: GetWordDataService) {}
+
+  ngOnInit(): void {
+    this.wordSubscription = this.getWordDataService.wordsData$.subscribe(
+      (events: Word) => {
+        this.collectionWord = events;
+      }
+    );
+    console.log(this.collectionWord);
+  }
+
+  ngOnDestroy(): void {
+    this.wordSubscription.unsubscribe();
+  }
 }
--- rslang/src/app/ebook/ebook.module.ts
+++ rslang/src/app/ebook/ebook.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
+
 import { EbookPageComponent } from './components/ebook-page/ebook-page.component';
 import { EbookRoutingModule } from './ebook-routing.module';
 
 @NgModule({
   declarations: [EbookPageComponent],
-  imports: [CommonModule, EbookRoutingModule],
+  imports: [SharedModule, EbookRoutingModule, HttpClientModule],
 })
 export class EbookModule {}
