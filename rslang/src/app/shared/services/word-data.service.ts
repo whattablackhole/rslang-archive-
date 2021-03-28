9bd23175d493f1b90c9649c7438df2a0673cfeb3
whattablackhole <rudolf.gritsenyak@gmail.com>
2021-03-28 23:03:32 +0300

    fix: add correct path

--- rslang/src/app/shared/services/word-data.service.ts
+++ rslang/src/app/shared/services/word-data.service.ts
@@ -2,10 +2,10 @@ import { Word } from '../models/word.model';
 import { Injectable } from '@angular/core';
 import { BaseDataService } from './base-data.service';
 import { HttpClient } from '@angular/common/http';
-
+import { BASE_URL } from '../constants/base-url';
 @Injectable()
 export class WordDataService extends BaseDataService<Word> {
-  getPath = (id: string) => `/words/${id}`;
+  getPath = (id: string) => `${BASE_URL}/words/${id}`;
 
   constructor(httpClient: HttpClient) {
     super(httpClient);
