4ca430c7c585753473fcaf1942815b30668afa3f
Malika <malikauku@gmail.com>
2021-03-28 21:13:29 +0700

    feat: add Angular Material tabs

--- rslang/src/app/material/material.module.ts
+++ rslang/src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTabsModule],
   exports: [CommonModule],
 })
 export class MaterialModule {}
