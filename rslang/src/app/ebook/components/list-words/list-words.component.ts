771555faa14a227673a7cb4277bcc86886c60cc3
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-03-28 23:56:14 +0300

    fix: service call

--- rslang/src/app/ebook/components/list-words/list-words.component.ts
+++ rslang/src/app/ebook/components/list-words/list-words.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { Word } from '../../../shared/models/word.model';
 
