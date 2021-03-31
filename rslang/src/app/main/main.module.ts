89face78b08a1d19a7179217990b2665b338282f
superpuper777 <superfevrik@gmail.com>
2021-04-01 01:19:12 +0300

    feat: add common styles variables and add promo section and about our team

--- rslang/src/app/main/main.module.ts
+++ rslang/src/app/main/main.module.ts
@@ -1,10 +1,12 @@
+import { MaterialModule } from './../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MainRoutingModule } from './main-routing.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [MainPageComponent],
-  imports: [CommonModule, MainRoutingModule],
+  imports: [CommonModule, MainRoutingModule, MaterialModule],
 })
 export class MainModule {}
--- rslang/src/app/material/material.module.ts
+++ rslang/src/app/material/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
-  exports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatCardModule],
+  exports: [CommonModule, MatButtonModule, MatIconModule, MatCardModule],
 })
 export class MaterialModule {}
