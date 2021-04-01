a6ba4a97661b05844899956a928648726a35d38b
superpuper777 <superfevrik@gmail.com>
2021-04-01 16:31:43 +0300

    refactor: separate main-page and team components and feat: add advantages section

--- rslang/src/app/main/main.module.ts
+++ rslang/src/app/main/main.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { TeamComponent } from './team/team';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, TeamComponent],
   imports: [CommonModule, MainRoutingModule, MaterialModule],
 })
 export class MainModule {}
---
+++ rslang/src/app/main/team/team.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  templateUrl: './team.html',
+  styleUrls: ['./team.scss'],
+})
+export class TeamComponent {}
