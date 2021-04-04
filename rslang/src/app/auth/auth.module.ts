86f6e7ad59ae61f7e496ae2c0d2a990522867e5a
Anna Tolstova <anna_tolstova@hotmail.com>
2021-04-04 14:44:24 +0300

    feat: add mat-form-fields

--- rslang/src/app/auth/auth.module.ts
+++ rslang/src/app/auth/auth.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { SharedModule } from '../shared/shared.module';
-import { AuthPage } from './components/auth-page/auth-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { AuthPage } from './components/auth-page/auth-page.component';
 import { AuthActionService } from './services/auth-action.service';
 
 @NgModule({
   declarations: [AuthPage],
-  imports: [CommonModule, AuthRoutingModule, SharedModule],
+  imports: [CommonModule, AuthRoutingModule, SharedModule, MatCardModule, MatFormFieldModule, MatInputModule],
   providers: [AuthActionService],
 })
 export class AuthModule {}
