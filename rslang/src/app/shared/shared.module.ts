b334016f3463ea9b7af122e4d3f64ecbf738e867
Anna Tolstova <anna_tolstova@hotmail.com>
2021-04-06 21:06:48 +0300

    fix: remove HttpClientModule from shared module

--- rslang/src/app/shared/shared.module.ts
+++ rslang/src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,7 +14,6 @@ import { Footer } from './components/footer/footer.component';
   declarations: [NotFoundPage, Footer],
   imports: [
     CommonModule,
-    HttpClientModule,
     RouterModule,
     MatMenuModule,
     MatToolbarModule,
