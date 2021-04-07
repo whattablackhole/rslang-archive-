42541c8a369d45ec5150bd67de9c90833ec0f287
Kastrubai Tatsiana <mitaka@tut.by>
2021-04-07 12:57:22 +0300

    fix: no-unused-vars

--- rslang/src/app/ebook/services/settings.service.ts
+++ rslang/src/app/ebook/services/settings.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { GlobalSettings } from '../models/global-settings.model';
 import { BaseDataService } from '../../core/services/base-data.service';
 
