a4386b3c1bc01df9080a3cd6a322b389f0d7235d
nofishtou <damnedbyhimself@gmail.com>
2021-04-08 21:09:32 +0300

    refactor: refactor code

--- rslang/src/app/games/savannah/components/savannah/savannah.component.ts
+++ rslang/src/app/games/savannah/components/savannah/savannah.component.ts
@@ -1,162 +1,201 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  trigger, style, animate, transition, state
+  trigger, style, animate, transition, state,
 } from '@angular/animations';
 import { Word } from 'src/app/shared/models/word.model';
 import { WordsDataService } from 'src/app/shared/services/words-data.service';
+import { GameCoreService } from 'src/app/games/services/game-core.service';
+import { WordWithStatistics } from 'src/app/shared/models/word-statistics.model';
+import { GameResults } from 'src/app/shared/models/game-results.model';
 
 @Component({
   selector: 'app-savannah',
   templateUrl: './savannah.component.html',
   styleUrls: ['./savannah.component.scss'],
-  providers: [ WordsDataService ],
+  providers: [WordsDataService, GameCoreService],
   animations: [
-    trigger('fallingDown', [
-      state('start', style({
-        top: '0%',
-      })),
-      state('end', style({
-        top: "100%"
-      })),
-      transition(
-        '* => end',
-        animate(4000),
+    trigger('fallingDownAnimation', [
+      state(
+        'start',
+        style({
+          top: '0%',
+        }),
       ),
-      transition(
-        '* => start',
-        animate(0),
+      state(
+        'end',
+        style({
+          top: '100%',
+        }),
       ),
-    ])
-  ]
+      transition('* => end', animate(4000)),
+      transition('* => start', animate(0)),
+    ]),
+  ],
 })
-
 export class Savannah implements OnInit {
   words: Word[];
+  gameResultWords: GameResults = {
+    correct_words: [],
+    incorrect_words: [],
+  };
+
+  correctGamePercent: number;
+
+  isGameStart = false;
+  isGameEnd = false;
   questionCounter = 1;
   streak = 0;
   biggestStreak = 0;
+  roundTimer = 4000;
+  answersAmount = 4;
   lives: boolean[] = [true, true, true];
+
   unUsedWords: Word[];
-  currentQuestion: Word;
+  currentWord: Word;
   currentAnswers: Word[] = [];
-  correctAnswers: Word[]= [];
-  wrongAnswers: Word[] = [];
-  isGameEnd: boolean = false;
-  fallingDownAnimationState: string = "start";
-  timer: any;
+  timer: ReturnType<typeof setTimeout>;
 
-  constructor(
-    private wordsDataService: WordsDataService,
-  ) {
+  fallingDownAnimationState = 'start';
 
-  }
+  constructor(private wordsDataService: WordsDataService, private gameCoreService: GameCoreService) {}
 
-  ngOnInit () {
+  ngOnInit(): void {
     this.words = this.wordsDataService.GetWords();
     this.unUsedWords = [...this.words];
-    this.generateRound();
-    console.log(this.wordsDataService.GetWords());
   }
 
-  generateRound() {
+  generateRound(): void {
     this.generateQuestion();
     this.generateAnswers();
+    this.currentAnswers = this.shuffleArray(this.currentAnswers);
     setTimeout(() => {
-      this.fallingDownAnimationState = "end";
-    }, 0)
+      this.fallingDownAnimationState = 'end';
+    }, 0);
     this.nextRoundTimer();
-    console.log(this.currentAnswers, "f")
   }
 
-  nextRoundTimer( ) {
-    if(this.questionCounter > this.words.length || !this.lives.length) {
-      this.isGameEnd = true;
-      if(this.biggestStreak < this.streak) {
-        this.biggestStreak = this.streak;
-        this.streak = 0;
-      }
-      console.log('end', this.correctAnswers, this.wrongAnswers);
+  startGame(): void {
+    this.isGameStart = true;
+    this.generateRound();
+  }
+
+  nextRoundTimer(): void {
+    if (this.questionCounter > this.words.length || !this.lives.length) {
+      this.onGameEnd();
       return;
     }
     this.timer = setTimeout(() => {
-      this.lives.splice( 0, 1);
-      this.currentAnswers.length = 0;
-      this.wrongAnswers.push(this.currentQuestion);
-      console.log('incorrect');
+      this.onWrongAnswer(this.currentWord);
       this.generateRound();
-      this.fallingDownAnimationState = "start";
-
-      console.log('this', this);
-      if(this.biggestStreak < this.streak) {
-        this.biggestStreak = this.streak;
-        this.streak = 0;
-      }
-
-    }, 4000)
-    console.log(this.timer);
+      this.fallingDownAnimationState = 'start';
+      this.calculateStreak();
+    }, this.roundTimer);
   }
 
-  checkAnswer(answer: Word) {
-    console.log(answer, this.fallingDownAnimationState);
-
-    if(answer.id === this.currentQuestion.id) {
-      console.log('correct');
-      // todo bug animatsii knopki
-
-      this.streak += 1;
-      this.currentAnswers.length = 0;
-      this.correctAnswers.push(this.currentQuestion);
-
+  checkAnswer(answer: Word): void {
+    if (answer.id === this.currentWord.id) {
+      this.onCorrectAnswer(this.currentWord);
     } else {
-      if(this.biggestStreak < this.streak) {
-        this.biggestStreak = this.streak;
-        this.streak = 0;
-      }
-      this.lives.splice( 0, 1);
-      this.currentAnswers.length = 0;
-      this.wrongAnswers.push(this.currentQuestion);
-      console.log('incorrect');
+      this.calculateStreak();
+      this.onWrongAnswer(this.currentWord);
     }
 
     clearTimeout(this.timer);
 
-    this.timer = null;
-    this.questionCounter +=1;
-    if(this.questionCounter > this.words.length || !this.lives.length) {
-      this.isGameEnd = true;
-      if(this.biggestStreak < this.streak) {
-        this.biggestStreak = this.streak;
-        this.streak = 0;
-      }
-      console.log('end', this.correctAnswers, this.wrongAnswers);
+    this.questionCounter += 1;
+    if (this.questionCounter > this.words.length || !this.lives.length) {
+      this.onGameEnd();
       return;
     }
 
     this.generateRound();
-    this.fallingDownAnimationState = "start";
-    return;
+    this.fallingDownAnimationState = 'start';
   }
 
-  generateQuestion() {
-    this.currentQuestion = this.unUsedWords.splice(this.randomNumber(0, this.unUsedWords.length), 1)[0];
+  generateQuestion(): void {
+    const [question] = this.unUsedWords.splice(this.randomNumber(0, this.unUsedWords.length), 1);
+    this.currentWord = question;
   }
 
-  generateAnswers() {
-    this.currentAnswers.push(this.currentQuestion);
-    while(this.currentAnswers.length !== 4 ) {
-      let answer = this.words[this.randomNumber(0, this.words.length)];
-
-      if(this.currentAnswers.some((a) => a.id === answer.id )) {
-        continue;
+  generateAnswers(): void {
+    this.currentAnswers.push(this.currentWord);
+    while (this.currentAnswers.length !== this.answersAmount) {
+      const answer: Word = this.words[this.randomNumber(0, this.words.length)];
+      if (!this.currentAnswers.some((a: Word) => a.id === answer.id)) {
+        this.currentAnswers.push(answer);
       }
-
-      this.currentAnswers.push(answer);
     }
   }
 
-  randomNumber(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+  randomNumber(min: number, max: number): number {
+    const minNumber = Math.ceil(min);
+    const maxNumber = Math.floor(max);
+    return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+  }
+
+  calculateStreak(): void {
+    if (this.biggestStreak < this.streak) {
+      this.biggestStreak = this.streak;
+      this.streak = 0;
+    }
+  }
+
+  makeWordAggregated(word: Word): WordWithStatistics {
+    return {
+      ...word,
+      isRemove: false,
+      isDifficult: false,
+      toStudy: {},
+      knowledgeDegree: 0,
+    };
+  }
+
+  changeWordKnowledge(word: WordWithStatistics, result: boolean): WordWithStatistics {
+    const changedWord = { ...word };
+    if (result) {
+      changedWord.knowledgeDegree += 1;
+    } else {
+      changedWord.knowledgeDegree -= 1;
+    }
+
+    return changedWord;
+  }
+
+  onWrongAnswer(currentWord: Word): void {
+    this.lives.splice(0, 1);
+    this.currentAnswers.length = 0;
+    const aggregatedWord = this.makeWordAggregated(currentWord);
+    this.gameResultWords.incorrect_words.push(this.changeWordKnowledge(aggregatedWord, false));
+  }
+
+  onCorrectAnswer(currentWord: Word): void {
+    this.streak += 1;
+    this.currentAnswers.length = 0;
+    const aggregatedWord = this.makeWordAggregated(currentWord);
+    this.gameResultWords.correct_words.push(this.changeWordKnowledge(aggregatedWord, true));
+  }
+
+  onGameEnd(): void {
+    this.isGameStart = false;
+    this.isGameEnd = true;
+    this.calculateStreak();
+    this.generateCorrectPercent();
+    this.gameCoreService.generateStats(this.gameResultWords, this.biggestStreak);
+  }
+
+  generateCorrectPercent(): void {
+    const correctNumber: number = this.gameResultWords.correct_words.length;
+    const incorrectNumber: number = this.gameResultWords.incorrect_words.length;
+    this.correctGamePercent = Math.floor((correctNumber * 100) / (incorrectNumber + correctNumber));
+  }
+
+  shuffleArray(arrToShuffle: Word[]): Word[] {
+    const arr = [...arrToShuffle];
+    for (let i = arr.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+
+    return arr;
   }
 }
--- rslang/src/app/games/savannah/savannah.module.ts
+++ rslang/src/app/games/savannah/savannah.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Savannah } from './components/savannah/savannah.component';
-import { SavannahRoutingModule } from './savannah-routing.module';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { Savannah } from './components/savannah/savannah.component';
+import { SavannahRoutingModule } from './savannah-routing.module';
 
 @NgModule({
   declarations: [Savannah],
-  imports: [CommonModule, SavannahRoutingModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    SavannahRoutingModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatIconModule],
 })
 export class SavannahModule {}
