26ca91b016801c5f20381acb0861258fab1df3c4
Anna Tolstova <anna_tolstova@hotmail.com>
2021-04-08 17:57:16 +0300

    fix: remove MatCardModule from auth

--- rslang/src/app/auth/auth.module.ts
+++ rslang/src/app/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { SharedModule } from '../shared/shared.module';
@@ -16,7 +15,6 @@ import { Register } from './components/register/register.component';
     CommonModule,
     AuthRoutingModule,
     SharedModule,
-    MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
