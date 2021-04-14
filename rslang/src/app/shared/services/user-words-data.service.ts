7e584faa3d652f1272b02e14bac21e92c4cc939a
whattablackhole <rudolf.gritsenyak@gmail.com>
2021-04-14 15:41:27 +0300

    fix: resolve constructor problem

--- rslang/src/app/shared/services/user-words-data.service.ts
+++ rslang/src/app/shared/services/user-words-data.service.ts
@@ -5,8 +5,7 @@ import { UserWord } from '../models/user-word.model';
 
 @Injectable()
 export class UserWordsDataService extends BaseDataService<UserWord[]> {
-  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
-  constructor(httpClient: HttpClient) {
+  public constructor(httpClient: HttpClient) {
     super(httpClient);
   }
 }
