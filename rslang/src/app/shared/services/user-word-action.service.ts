3da42042c0c49c9a727dc7d9149ece3fc59ab8c3
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-04-15 11:12:29 +0300

    feat: add functionality for user words

---
+++ rslang/src/app/shared/services/user-word-action.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { BaseActionService } from '../../core/services/base-action.service';
+// import { NotificationService } from '../../shared/services/notification.service';
+import { CallbackObject } from '../models/callback-object.model';
+import { CreateUserWordRequest } from '../models/create-user-word-request.model';
+import { API_URL } from '../constants/api-url';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserWordActionService extends BaseActionService {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(
+    httpClient: HttpClient,
+    // private notifyService: NotificationService,
+  ) {
+    super(httpClient);
+  }
+
+  createUserWord(userId: string, wordId: string, word: CreateUserWordRequest): void {
+    const action: CallbackObject = {
+      onError(err) {
+        const message = (err.status === 400)
+          ? 'Bad request'
+          : 'Please try again.';
+        console.error(message);
+        // notifyService.showError(message);
+      },
+    };
+    this.sendAction('POST', API_URL.USER_WORD_BY_ID(userId, wordId), action, { body: word });
+  }
+
+  updateUserWord(userId: string, wordId: string, word: CreateUserWordRequest): void {
+    const action: CallbackObject = {
+      onError(err) {
+        const message = (err.status === 400)
+          ? 'Bad request'
+          : 'Please try again.';
+        console.error(message);
+        // notifyService.showError(message);
+      },
+    };
+    this.sendAction('PUT', API_URL.USER_WORD_BY_ID(userId, wordId), action, { body: word });
+  }
+
+  deleteUserWord(userId: string, wordId: string): void {
+    const action: CallbackObject = {
+      onSuccess() {
+        const message = 'The word has been deleted';
+        console.log(message);
+        // notifyService.showMessage(message);
+      },
+      onError(err) {
+        const message = (err.status === 401)
+          ? 'Access token is missing or invalid'
+          : 'Please try again.';
+        console.error(message);
+        // notifyService.showError(message);
+      },
+    };
+    this.sendAction('PUT', API_URL.USER_WORD_BY_ID(userId, wordId), action);
+  }
+}
